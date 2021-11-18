8f8c1589af4c6962d816cd923194febb75be8a03
nikunj shukla <nas.nikunj@gmail.com>
2021-11-18 21:28:21 +0530

    minor fix

--- alarms.tf
+++ alarms.tf
@@ -1,5 +1,9 @@
+locals {
+  target_group_name = element(module.alb.target_group_arn_suffixes, 0)
+}
+
 resource "aws_cloudwatch_metric_alarm" "target_response_time_average" {
-  alarm_name          = "alb-tg-${element(module.alb.target_group_arns, 0)}-highResponseTime"
+  alarm_name          = "alb-tg-${local.target_group_name}-highResponseTime"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 5
   metric_name         = "TargetResponseTime"
@@ -13,6 +17,44 @@ resource "aws_cloudwatch_metric_alarm" "target_response_time_average" {
 
   dimensions = {
     "LoadBalancer" = module.alb.this_lb_arn_suffix
+    "TargetGroup"  = local.target_group_name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "httpcode_lb_5xx_count" {
+  alarm_name          = "alb-${module.alb.this_lb_arn_suffix}-high5XXCount"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 5
+  metric_name         = "HTTPCode_ELB_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = "5"
+  alarm_description   = "Average API 5XX load balancer error code count is too high"
+  alarm_actions       = ["${aws_sns_topic.refinery_alb_alert.id}"]
+  ok_actions          = ["${aws_sns_topic.refinery_alb_alert.id}"]
+
+  dimensions = {
+    "LoadBalancer" = module.alb.this_lb_arn_suffix
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "unhealthy_host_count" {
+  alarm_name          = "tg-${local.target_group_name}-unhealthyHostCount"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 5
+  metric_name         = "UnHealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 0
+  alarm_description   = "Unhealthy host count behind ECS is more than or equal to 1"
+  alarm_actions       = ["${aws_sns_topic.refinery_alb_alert.id}"]
+  ok_actions          = ["${aws_sns_topic.refinery_alb_alert.id}"]
+
+  dimensions = {
+    "LoadBalancer" = module.alb.this_lb_arn_suffix
+    "TargetGroup"  = local.target_group_name
   }
 }
 
